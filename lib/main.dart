181b7d0154f8c092a38edbe67be59e1f8240a797
manpits <manpits@gmail.com>
2025-03-01 12:53:37 +0800

    dart-data-type

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 /* --------------------------------------------
-Disable debug banner
-
-Widget MaterialApp memiliki property 'debugShowCheckedModeBanner' yang bisa diset false untuk menonaktifkan debug banner
+Tipe data dalam bahasa pemrograman Dart
 -----------------------------------------------*/
 
 import 'package:flutter/material.dart';
@@ -9,6 +7,31 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
+// Beberapa jenis tipe data bahasa pemrograman DART
+String name = "Pemrograman Mobile dengan Flutter";
+int bulat = 10;
+double desimal = 99.3;
+bool kondisi = false;
+List array = ["Satu", "Dua", "Tiga"];
+
+Map<String, String> mapStringToString = {
+  "Satu": "One",
+  "Dua": "Two",
+  "Tiga": "Three",
+};
+
+Map<String, int> mapStringToInteger = {
+  "Satu": 1,
+  "Dua": 2,
+  "Tiga": 3,
+};
+
+Map<String, dynamic> mapStringToDynamic = {
+  "Satu": "One",
+  "Dua": 2,
+  "Tiga": 3.0,
+};
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -22,7 +45,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('My App'),
         ),
-        body: const Text('My first app'),
+        body: Text(name),
       ),
     );
   }
