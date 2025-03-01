95f9e44667fe0ac2963ef8ee7c549673ff87dba2
manpits <manpits@gmail.com>
2025-03-01 12:31:45 +0800

    first-project

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
+/* --------------------------------------------
+1. Import library material.dart
+2. Buat function 
+  void main() {
+    runApp(const MyApp)
+  }
+
+3. Buat sebuah stateless widget dengan nama 'MyApp'
+4. Pada method build dari widget 'MyApp', lakukan return widget 'MaterialApp'
+5. Tempatkan widget Scaffold pada property 'home' dari widget 'MaterialApp'
+6. Widget Scaffold memiliki properti 'appBar', coba isikan dengan sebuah widget AppBar
+7. Widget Scaffold memiliki properti 'body', coba isikan dengan sebuah widget Text
+-----------------------------------------------*/
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('My App'),
         ),
+        body: const Text('My first app'),
       ),
     );
   }
