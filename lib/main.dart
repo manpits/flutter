8518ab9f2d9e2f2ca1f8375b89a2ccaec11f8815
manpits <manpits@gmail.com>
2025-03-01 22:18:02 +0800

    -1740838682

--- lib/main.dart
+++ lib/main.dart
@@ -20,45 +20,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Widget Column'),
         ),
-        body: Container(
-          color: Colors.grey,
-          // -----------------------------------------------------------------
-          // Aktifkan property width: double.infinity untuk melihat cara kerja
-          // property 'crossAxisAlignment'
-          // -----------------------------------------------------------------
-          //height: double.infinity,
-          //
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Container(
-                width: 100.0,
-                height: 100.0,
-                margin: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.amber,
-                ),
-                child: Center(
-                  child: Text('Container 1'),
-                ),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                margin: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.blue,
-                ),
-                child: Center(
-                  child: Text('Container 2'),
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: Image.asset('assets/images/bg.jpg'),
       ),
     );
   }
