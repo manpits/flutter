a10fb9c1befcbd53603a126e4b09508458d5e8f3
manpits <manpits@gmail.com>
2025-03-01 22:57:44 +0800

    -1740841064

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 /* --------------------------------------------
-Widget Image
+Widget Stack
 -----------------------------------------------*/
 
 import 'package:flutter/material.dart';
@@ -18,27 +18,32 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Widget Image'),
+          title: Text('Widget Stack'),
         ),
         /**
-         * download gambar dari internet, misalnya 'bg.jpg'
-         * simpan gambar 'bg.jpg' ke folder 'assets/image'
-         * edit 'pubspec.yaml' , tambahkan
-         *  flutter:
-         *    ...
-         *    assets:
-         *      - assets/images/
-         * 
-         *  Wrap widget Image dengan widget Container untuk melihat cara kerja property 'fit'
+         * Umumnya sebuah widget memiliki child, namun jika widget tidak memiliki child, kita bisa menambahkan
+         * widget Stack, untuk menampilkan widget diatasnya
+         * Sebagai contoh, widget Image tidak memiliki child, sehingga kita bisa menggunakan widget Stack untuk
+         * menampilkan widget Text diatasnya
+         * Tambahkan widget Container untuk mengatur posisi tampilan widget Text diatas widget Image
          */
-        body: Container(
-          color: Colors.white30,
-          height: double.infinity,
-          child: Image.asset(
+        body: Stack(children: [
+          Image.asset(
             'assets/images/bg.jpg',
-            //fit: BoxFit.cover,
+            fit: BoxFit.cover,
           ),
-        ),
+          Container(
+            width: double.infinity,
+            height: 50.0,
+            color: Colors.teal,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('This text is stacked over image !'),
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
