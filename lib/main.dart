3293917ed3b91f978e2871b77d5e72e030dc0285
manpits <manpits@gmail.com>
2025-03-02 09:39:17 +0800

    -1740879557

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 /* --------------------------------------------
-Widget FloatingActionButton
+Widget Drawer
 -----------------------------------------------*/
 
 import 'package:flutter/material.dart';
@@ -15,17 +15,59 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(brightness: Brightness.light),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Widget FloatingActionButton'),
+          title: const Text('Widget Drawer'),
         ),
         /**
-         * Pada widget Scaffold dapat ditambahkan property 'floatingActionButton'
-         * method 'onPressed' digunakan untuk menangkap event tap pada widget FloatingActionButton
-         * wrap widget 'FloatingActionButton' dengan widget 'Column' untuk membuat lebih dari 1 elemen 'FloatingActionButton'
-         * set property 'mainAxisSize:MainAxisSize.min' untuk mengatur agar ukuran Column hanya seukuran widget didalamnya
+         * Pada widget Scaffold dapat ditambahkan property 'drawer' dengan widget 'Drawer' untuk menempatkan semacam sidebar menu
          */
+        drawer: Drawer(
+          child: Column(
+            children: [
+              DrawerHeader(
+                child: Column(
+                  children: [
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'assets/images/bg.jpg',
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const Text('Menu Utama'),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.home),
+                    title: Text('Home'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('Profile'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Pengaturan'),
+                    onTap: () {},
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
         floatingActionButton: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
