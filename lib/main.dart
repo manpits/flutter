b216d7ad688c1adeb4d2064dfc33233b296d9c31
manpits <manpits@gmail.com>
2025-03-02 23:06:26 +0800

    021-light-dark-theme-switching-1740927986

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_003_empty/views/data/notifiers.dart';
 import 'package:flutter_003_empty/views/widget_tree.dart';
-import 'package:flutter_003_empty/views/widgets/navbarwidget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,26 +23,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           theme:
               ThemeData(brightness: value ? Brightness.dark : Brightness.light),
-          home: ValueListenableBuilder(
-            valueListenable: selectedThemeNotifier,
-            builder: (context, value, child) {
-              return Scaffold(
-                appBar: AppBar(
-                  title: value ? Text('Mode Malam') : Text('Mode Siang'),
-                  actions: [
-                    IconButton(
-                        onPressed: () {
-                          selectedThemeNotifier.value =
-                              !selectedThemeNotifier.value;
-                        },
-                        icon: Icon(value ? Icons.light_mode : Icons.dark_mode))
-                  ],
-                ),
-                body: const WidgetTree(),
-                bottomNavigationBar: const NavbarWidget(),
-              );
-            },
-          ),
+          home: const WidgetTree(),
         );
       },
     );
--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_003_empty/views/data/notifiers.dart';
 import 'package:flutter_003_empty/views/pages/homepage.dart';
 import 'package:flutter_003_empty/views/pages/profilepage.dart';
+import 'package:flutter_003_empty/views/widgets/navbarwidget.dart';
 
 List<Widget> pages = const [
   Homepage(),
@@ -14,9 +15,27 @@ class WidgetTree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
-      valueListenable: selectedPageNotifier,
+      valueListenable: selectedThemeNotifier,
       builder: (context, value, child) {
-        return pages.elementAt(value);
+        return Scaffold(
+          appBar: AppBar(
+            title: value ? const Text('Mode Malam') : const Text('Mode Siang'),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    selectedThemeNotifier.value = !selectedThemeNotifier.value;
+                  },
+                  icon: Icon(value ? Icons.light_mode : Icons.dark_mode))
+            ],
+          ),
+          body: ValueListenableBuilder(
+            valueListenable: selectedPageNotifier,
+            builder: (context, value, child) {
+              return pages.elementAt(value);
+            },
+          ),
+          bottomNavigationBar: const NavbarWidget(),
+        );
       },
     );
   }
