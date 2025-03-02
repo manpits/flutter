175ec92067333448539286b54ff339b190123f26
manpits <manpits@gmail.com>
2025-03-02 11:10:59 +0800

    -1740885059

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,11 @@ void main() {
   runApp(const MyApp());
 }
 
+/// Tanda ? pada String? textData = 'Testing null safety' berarti
+/// variable 'textData' bisa null atau String
+/// Bedakan antara null dengan String ""
+String? textData = 'Testing null safety';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -18,102 +23,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(brightness: Brightness.light),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Widget SafeArea'),
+          title: const Text('Null safety'),
         ),
-        /**
-         * Widget 'Drawer' kita dapat wrap dengan widget SafeArea
-         * Widget SafeArea akan membuat widget yang kita buat tampilannya tidak melebihi bagian atas atau bagian bawah layar
-         * Untuk melihat pengaruh widget SafeArea, coba jalankan aplikasi pada emulator Android atau ioS atau langsung pada perangkat mobile
-         */
-        drawer: SafeArea(
-          child: Drawer(
-            child: Column(
-              children: [
-                Column(
-                  children: [
-                    ListTile(
-                      leading: const Icon(Icons.home),
-                      title: const Text('Home'),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.person),
-                      title: const Text('Profile'),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.settings),
-                      title: const Text('Pengaturan'),
-                      onTap: () {},
-                    ),
-                  ],
-                ),
-                const Expanded(
-                  child: SizedBox(),
-                ),
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Divider(
-                        color: Colors.grey[0],
-                        height: 5,
-                        thickness: 0.5,
-                      ),
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.logout),
-                      title: const Text('Logout'),
-                      onTap: () {},
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
-        body: const Center(
-          child: Text('Widget SafeArea'),
-        ),
-        floatingActionButton: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                print('FloatingActionButton + is clicked !');
-              },
-              child: const Icon(
-                Icons.add,
-              ),
-            ),
-            const SizedBox(
-              height: 2.0,
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                print('FloatingActionButton - is clicked !');
-              },
-              child: const Icon(
-                Icons.remove,
-              ),
-            ),
-          ],
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          selectedIndex: 1,
-          onDestinationSelected: (value) {
-            print(value);
-          },
+        body: Center(
+          /**
+           * Tanda ! pada textData! untuk memastikan bahwa nilai textData tidak null
+           * Jika nilai textData = null maka akan memunculkan error 
+           */
+          child: Text(textData!),
         ),
       ),
     );
