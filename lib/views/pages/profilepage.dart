5afe743130fe88707c4e5b046c4a86b6f1908b61
manpits <manpits@gmail.com>
2025-03-03 13:27:01 +0800

    023-checkbox-widget-1740979621

--- lib/views/pages/profilepage.dart
+++ lib/views/pages/profilepage.dart
@@ -9,6 +9,7 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   TextEditingController textController = TextEditingController();
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +31,38 @@ class _ProfilePageState extends State<ProfilePage> {
             height: 5,
           ),
           Text('Teks : ${textController.text}'),
+          const SizedBox(
+            height: 5,
+          ),
+          Row(
+            children: [
+              /**
+               * Checkbox widget
+               */
+              Checkbox(
+                value: isChecked,
+                onChanged: (value) {
+                  setState(() {
+                    isChecked = !isChecked;
+                  });
+                },
+              ),
+              Text('Check box')
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          CheckboxListTile(
+            value: isChecked,
+            onChanged: (value) {
+              setState(() {
+                isChecked = !isChecked;
+              });
+            },
+            title: Text('Checkbox Listtile'),
+            tileColor: Colors.red,
+          ),
         ],
       ),
     );
